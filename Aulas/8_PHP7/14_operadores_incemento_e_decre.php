5851f38845bb8cb99492de9bb4e0ae566b8687fd
João Inácio <joaoinacio206@protonmail.com>
2021-03-17 16:19:48 -0300

    Create 14_operadores_incemento_e_decre.php

---
+++ Aulas/8_PHP7/14_operadores_incemento_e_decre.php
@@ -0,0 +1,24 @@
+<html lang='pt-br'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='style.css'>
+        <title>Operadores de incremento/ decremento</title>
+    </head>
+    <body>
+        <?php
+            
+            $n1 = 5;
+            echo $n1;
+            echo '<br />';
+            $n1++;
+            echo $n1;
+            echo '<br />';
+            ++$n1;
+            echo $n1;
+
+            
+        ?>
+    </body>
+</html>
