64210b33d9e37a08930a7aee7a2afff5e529ef4f
João Inácio <joaoinacio206@protonmail.com>
2021-03-22 10:41:18 -0300

    Create 17_funcoes_Pr_matm.php

---
+++ Aulas/8_PHP7/17_funcoes_Pr_matm.php
@@ -0,0 +1,21 @@
+<html lang='pt-br'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='style.css'>
+        <title>Funções nativas para tarefas matemáticas</title>
+    </head>
+    <body>
+    <h1>Funções nativas para tarefas matemáticas</h1>
+        <?php
+            $num = 7.3;
+            // echo ceil($num) // arredondamento para cima
+            // echo floor($num) // arredondamento para baixo
+            // echo round($num) // arredondamento com base na fração
+            // echo rand(5, 50) ;//Gera um numero aleatorio
+            // echo '<br />'. getrandmax();
+            echo sqrt(180)
+        ?>
+    </body>
+</html>
