9650eb5b90645ce642169b20ff509e9acf26cc43
João Inácio <joaoinacio206@protonmail.com>
2021-03-29 18:33:04 -0300

    Create 25_Do_while.php

---
+++ Aulas/8_PHP7/25_Do_while.php
@@ -0,0 +1,20 @@
+<html lang='pt-br'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='style.css'>
+        <title>Do While</title>
+    </head>
+    <body>
+        <h1>Do While</h1>
+        <?php
+            $x = 10;
+            do {
+                echo 'Entrou no while';
+            } while ($x < 9);
+            
+            
+        ?>
+    </body>
+</html>
