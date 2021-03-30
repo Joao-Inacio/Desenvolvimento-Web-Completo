ae0adfefc5a198c693eba929ab880678f8e8055f
João Inácio <joaoinacio206@protonmail.com>
2021-03-30 09:56:27 -0300

    Create 26_For_while.php

---
+++ Aulas/8_PHP7/26_For_while.php
@@ -0,0 +1,19 @@
+<html lang='pt-br'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='style.css'>
+        <title>For While</title>
+    </head>
+    <body>
+        <h1>For While</h1>
+        <?php
+            
+            for ($i = 1; $i <= 10 ; $i++) { 
+                echo "$i <br>";
+            }
+            
+        ?>
+    </body>
+</html>
