c0f7702f9a2ed9b9c659ab60703ec2fb3ab9428f
João Inácio <joaoinacio206@protonmail.com>
2021-04-08 09:04:45 -0300

    Update abrir_chamado.php

--- Projetos/Projeto App Help Desk/app_help_desk/abrir_chamado.php
+++ Projetos/Projeto App Help Desk/app_help_desk/abrir_chamado.php
@@ -1,84 +1,76 @@
 <? require_once "validador_acesso.php" ?>
 <html lang='pt-br'>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-      .card-abrir-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home.php">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
-      </a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="logoff.php">SAIR</a>
-        </li>
-      </ul>
-    </nav>
-
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-abrir-chamado">
-          <div class="card">
-            <div class="card-header">
-              Abertura de chamado
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  
-                  <form>
-                    <div class="form-group">
-                      <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='style.css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <style>
+            .card-abrir_chamado{
+                padding: 30px 0 0 0;
+                width: 100%;
+                margin: 0 auto;
+            }
+        </style>
+        <title>App Help Desk</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <a href="home.php">
+                <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                App Help Desk
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="logoff.php">SAIR</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="card-abrir_chamado">
+                    <div class="card">
+                        <div class="card-header">
+                            Abertura de chamado
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <form action="">
+                                        <div class="form-group">
+                                            <label for="">Título</label>
+                                            <input type="text" class="form-control" placeholder="Título">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Categoria</label>
+                                            <select class="form-control" name="" id="">
+                                                <option value="">Criação Usuário</option>
+                                                <option value="">Impressora</option>
+                                                <option value="">Hardware</option>
+                                                <option value="">Software</option>
+                                                <option value="">Rede</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Descrição</label>
+                                            <textarea class="form-control" name="" id="" cols="30" rows="3"></textarea>
+                                        </div>
+                                        <div class="row mt-5">
+                                            <div class="col-6">
+                                                <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
+                                            </div>
+                                            <div class="col-6">
+                                            <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    
-                    <div class="form-group">
-                      <label>Categoria</label>
-                      <select class="form-control">
-                        <option>Criação Usuário</option>
-                        <option>Impressora</option>
-                        <option>Hardware</option>
-                        <option>Software</option>
-                        <option>Rede</option>
-                      </select>
-                    </div>
-                    
-                    <div class="form-group">
-                      <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
-                    </div>
-
-                    <div class="row mt-5">
-                      <div class="col-6">
-                          <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
-                      </div>
-
-                      <div class="col-6">
-                        <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
-                      </div>
-                    </div>
-                  </form>
-
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-    </div>
-  </body>
+    </body>
 </html>
