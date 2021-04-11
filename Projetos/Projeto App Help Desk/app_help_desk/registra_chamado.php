641560b7584df3527382f301a671414514b83464
João Inácio <joaoinacio206@protonmail.com>
2021-04-11 10:52:28 -0300

    Controle de usuários

--- Projetos/Projeto App Help Desk/app_help_desk/registra_chamado.php
+++ Projetos/Projeto App Help Desk/app_help_desk/registra_chamado.php
@@ -1,19 +1,20 @@
 <?php
 
+    session_start();
+
     $titulo = str_replace('#', '-', $_POST['titulo']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
     $arquivo = fopen('arquivo.txt', 'a');
-
-    $titulo = str_replace('#', '-', $_POST['titulo']);
-    $categoria = str_replace('#', '-', $_POST['categoria']);
-    $descricao = str_replace('#', '-', $_POST['descricao']);
-    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
     fwrite($arquivo,  $texto);
 
     fclose($arquivo);
+
+    header('Location: abrir_chamado.php')
     
 
 
--- Projetos/Projeto App Help Desk/app_help_desk/valida_login.php
+++ Projetos/Projeto App Help Desk/app_help_desk/valida_login.php
@@ -2,9 +2,15 @@
     session_start();
     
     $usuarios_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+    $perfis = array(1 => 'administrativo', 2 => 'Usuário');
+
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br' , 'senha' => '123456' ),
-        array('email' => 'user@teste.com.br' , 'senha' => 'abcde' )
+        array('id' => 1 , 'email' => 'adm@teste.com.br' , 'senha' => '1020304050' , 'perfil_id' => 1),
+        array('id' => 2 , 'email' => 'user@teste.com.br' , 'senha' => 'abcde' , 'perfil_id' => 1),
+        array('id' => 3 , 'email' => 'jose@teste.com.br' , 'senha' => 'abcde' , 'perfil_id' => 2),
+        array('id' => 4 , 'email' => 'maria@teste.com.br' , 'senha' => 'abcde' , 'perfil_id' =>  2)
     );
     /*echo '<pre>';
     print_r($usuarios_app);
@@ -13,12 +19,16 @@
     foreach($usuarios_app as $user){
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
         }
     }
         if($usuario_autenticado){
             echo 'Usuário autenticado.';
     
             $_SESSION['autenticado'] = 'SIM';
+            $_SESSION['id'] = $usuario_id;
+            $_SESSION['perfil_id'] = $usuario_perfil_id;
             header('Location: home.php');
         }else{  
             $_SESSION['autenticado'] = 'NÃO';
