4091abee4abf7513768afdda889542fb568e2e31
João Inácio <joaoinacio206@protonmail.com>
2021-04-23 14:13:21 -0300

    Update processa_envio.php

--- Projetos/10-Projeto-App-Send-Mail/processa_envio.php
+++ Projetos/10-Projeto-App-Send-Mail/processa_envio.php
@@ -1,5 +1,34 @@
 <?php
     
-    
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+            return $this->atributo;
+        }
+        public function __set($atributo, $valor){
+            $this->atributo = $valor;
+        }
+        public function mensagemValida(){
+            if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+            return true;
+        }
+
+    }
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    if ($mensagem->mensagemValida()) {
+        
+    }
+
+
     
 ?>
