d13f7b8be13e90e4c4ddaf639d4f049658cd7c93
João Inácio <joaoinacio206@protonmail.com>
2021-04-24 09:30:46 -0300

    Implementação do PHPMailer

--- Projetos/10-Projeto-App-Send-Mail/processa_envio.php
+++ Projetos/10-Projeto-App-Send-Mail/processa_envio.php
@@ -1,4 +1,12 @@
 <?php
+    require "./Biblioteca/PHPMailer/Exception.php";
+    require "./Biblioteca/PHPMailer/OAuth.php";
+    require "./Biblioteca/PHPMailer/PHPMailer.php";
+    require "./Biblioteca/PHPMailer/POP3.php";
+    require "./Biblioteca/PHPMailer/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
     
     class Mensagem {
         private $para = null;
@@ -27,7 +35,45 @@
 
     if ($mensagem->mensagemValida()) {
         
+    }else {
+        
     }
+    $mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Não foi possível enviar este -email!';
+    echo 'Detalhes do erro ' . $mail->ErrorInfo;
+}
 
 
     
