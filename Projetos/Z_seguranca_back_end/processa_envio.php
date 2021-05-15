42e489de0c255177970ffa213df98e4484f77c74
João Inácio <joaoinacio206@protonmail.com>
2021-05-15 13:24:08 -0300

    Update processa_envio.php

--- Projetos/Z_seguranca_back_end/processa_envio.php
+++ Projetos/Z_seguranca_back_end/processa_envio.php
@@ -49,7 +49,7 @@
 	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
 	    $mail->Username = 'joaonetoplus@gmail.com';                 // SMTP username
-	    $mail->Password = 'plus021998';                           // SMTP password
+	    $mail->Password = '';                           // SMTP password
 	    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	    $mail->Port = 587;                                    // TCP port to connect to
 
