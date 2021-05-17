96c7df8183f1fed822f030f5a045170fa29b248d
João Inácio <joaoinacio206@protonmail.com>
2021-05-17 19:01:52 -0300

    up

--- Aulas/10_PHP_PDO_MYSQL/index.php
+++ Aulas/10_PHP_PDO_MYSQL/index.php
@@ -2,6 +2,34 @@
     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuarios = 'root';
     $senha = '';
-    $conexao = new PDO($dsn, $usuarios, $senha);
+
+    try {
+        $conexao = new PDO($dsn, $usuarios, $senha);
+
+        $query = '
+            create table tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ';
+
+        $conexao->exec($query);
+
+        $query = '
+                insert into tb_usuarios(
+                    nome, email, senha
+                ) values("João Inácio", "joaoinacio@teste.com.br", "123456")
+        ';
+        $conexao->exec($query);
+
+
+    } catch (PDOException $e) {
+        echo '<pre>';
+        print_r($e);
+        echo'</pre>';
+    }
+
     
 ?>
