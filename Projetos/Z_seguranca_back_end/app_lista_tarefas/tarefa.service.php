4f39b51e208eeed7580583bf2083da30d6adf9a5
João Inácio <joaoinacio206@protonmail.com>
2021-05-21 09:53:56 -0300

    Create tarefa.service.php

---
+++ Projetos/Z_seguranca_back_end/app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,20 @@
+<?php
+    class TarefaService {
+        public function inserir() {
+
+        }
+        public function recuperar() {
+
+        }
+        public function atualizar() {
+
+        }
+        public function remover() {
+
+        }
+        
+
+    }
+    
+    
+?>
