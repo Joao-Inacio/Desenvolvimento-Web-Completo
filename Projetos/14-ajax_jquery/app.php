2e0993413a2e3faf1e346c870f0d7ec76ec2808e
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2021-12-17 11:17:30 -0300

    Utilizando o método $.ajax()

--- Projetos/14-ajax_jquery/app.php
+++ Projetos/14-ajax_jquery/app.php
@@ -96,15 +96,21 @@ $dashboard = new Dashboard();
 
 $conexao = new Conexao();
 
-$dashboard->__set('data_inicio', '2018-10-01');
-$dashboard->__set('data_fim', '2018-10-31');
+$competencia = explode('-', $_GET['competencia']);
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+$dashboard->__set('data_inicio', $ano. '-'.$mes.'-01');
+$dashboard->__set('data_fim', $ano. '-'.$mes.'-'.$dias_do_mes);
 
 
 $bd = new Bd($conexao, $dashboard);
 
 $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
 $dashboard->__set('totalVendas', $bd->getTotalVendas());
-print_r($dashboard);
+echo json_encode($dashboard);
 
 
 ?>
--- Projetos/14-ajax_jquery/script.js
+++ Projetos/14-ajax_jquery/script.js
@@ -5,4 +5,19 @@ $(document).ready(() => {
 	$('#suporte').on('click', () => {
         $('#pagina').load('suporte.html')
     })
+    // Ajax
+    $('#competencia').on('change', e =>{
+        let competencia = $(e.target).val()
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html(dados.totalVendas)
+                console.log('Requisição realizada com sucesso')},
+            error: () => {console.log('erro')},
+        })
+    })
 })
